8e1048a7543efea3f571ee79aa89b5bd4c0a44ef
lay595715148 <lay595715148@126.com>
2013-12-23 17:14:59 +0800

    update to view demo

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-var app = require('express')(), 
-    server = require('http').createServer(app), 
-    io = require('socket.io').listen(server);
+var http = require('http');
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(8132);
 
+app.configure(function() {
+    app.set('views', __dirname + '/template');
+    app.set('view engine', 'jade');
+    app.use("/css", express.static(__dirname + '/static/css'));
+    app.use("/jquery", express.static(__dirname + '/static/jquery'));
+    app.use("/js", express.static(__dirname + '/static/js'));
+    app.use("/image", express.static(__dirname + '/static/image'));
+
+    app.use(express.logger({stream: fs.createWriteStream(__dirname + '/logs/express.log', {flags: 'a'})}));
+    //app.use(app.router);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.cookieSession({ secret:'laychat',cookie: { maxAge: 60 * 60 * 1000 }}));
+    app.use(express.static(__dirname + '/static'));
+    
+});
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
+});
+app.get('/:t', function(req, res) {
+    io.sockets.on('connection', function(socket) {
+        socket.emit('news', {
+            content : req.params.t
+        });
+        console.log('OK');
+    });
+    res.send('OK');
 });
 
 io.sockets.on('connection', function(socket) {
