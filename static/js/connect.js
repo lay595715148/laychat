83b49691f45b7c3768d269866c4ab77acf105e87
lay595715148 <lay595715148@126.com>
2013-12-26 16:04:33 +0800

    add jquery plugin pnotify

---
+++ static/js/connect.js
@@ -0,0 +1,17 @@
+/**
+ * socket cnnect file
+ */
+$(document).ready(function() {
+    var socket = io.connect('http://localhost:8132/');
+    socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+    }).on('connect_failed', function(err) {
+        console.log('err');
+        console.log(err);
+    }).on('disconnect', function() {
+        console.log('disconnect');
+    }).on('reconnecting', function() {
+        console.log('reconnecting');
+    });
+});
